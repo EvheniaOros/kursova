d44d656d35d36c4c97f6ba7d69777800d74617da
OROSEV <zhenka-oros@mail.ru>
2019-05-04 18:32:39 +0300

    forms

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,17 @@
 class EmployeesController < ApplicationController
+	before_action :authenticate_user!
   def index
+  end
+  def new
+  end
+  def create
+  end
+  def edit
+  end
+  def show
+  end
+  def update
+  end
+  def destroy
   end
 end
--- app/controllers/employeesparams_controller.rb
+++ app/controllers/employeesparams_controller.rb
@@ -1,4 +1,19 @@
 class EmployeesparamsController < ApplicationController
+	before_action :authenticate_user!
   def index
   end
-end
+   def new
+   	@employeesparam = Employeesparam.new
+  end
+  def create
+
+    end  
+  end
+  def edit
+  end
+  def show
+  end
+  def update
+  end
+  def destroy
+  end
